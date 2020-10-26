6b0f4fa2dfa7646eb57307f95a0219ff97399daf
ru6300 <ru6300@naver.com>
2020-10-26 13:36:08 +0900

    Change UserInfoDAO

--- play/src/main/java/com/ssafy/play/dao/UserInfoDAO.java
+++ play/src/main/java/com/ssafy/play/dao/UserInfoDAO.java
@@ -14,8 +14,6 @@ public interface UserInfoDAO {
 	public int insertUserInfo(UserInfo userinfo);
 	public int updateUserInfo(UserInfo userinfo);
 	public int deleteUserInfo(int user_id);
-	public UserInfo findUserInfoByEmail(String email);
-	public UserInfo findUserByEmailAndPassword(String email, String password);
 	public UserInfo findUserInfoByNickname(String nickname);
 	public List<UserInfo> selectUserInfoSearch(String keyword);
 	public String findNicknameById(int user_id);
@@ -23,8 +21,6 @@ public interface UserInfoDAO {
 	public int updateImage(UserInfo userinfo);
 	public int updatePassword(UserInfo userinfo);
 	public int updateDisable(int user_id);
-	public int updateRole(int user_id);
+	public int updateLike(int user_id);
 	public String selectSaltByUserId(int user_id);
-	public int updateable(int user_id);
-	public int deleteRole(int user_id);
 }
