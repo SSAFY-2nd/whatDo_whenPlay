50c87d881ef9cbab460e3edf50105185e832675a
임건호 <ghlim001@naver.com>
2020-10-29 17:39:31 +0900

    S03P31A304-31 Gunho: add list

---
+++ Frontend/LoginRegisterExample/app/src/main/java/com/example/loginregisterexample/FoodCategory.java
@@ -0,0 +1,38 @@
+package com.example.list;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+
+public class FoodCategory extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.foodcategory);
+
+        GridView gridView = (GridView)findViewById(R.id.action_food);
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(FoodCategory.this, ""+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
+//    {
+//        view = inflater.inflate(R.layout.foodcategory,container,false);
+//
+//        return view;
+//    }
+}
---
+++ Frontend/LoginRegisterExample/app/src/main/java/com/example/loginregisterexample/ListActivity.java
@@ -0,0 +1,76 @@
+package com.example.list;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity
+{
+    private BottomNavigationView bottomNavigationView; // 바텀 네비게이션 뷰
+    private FragmentManager fm;
+    private FragmentTransaction ft;
+    private FoodCategory frag1;
+    private PlayCategory frag2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavi);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem)
+            {
+
+                switch (menuItem.getItemId())
+                {
+//                    System.out.println(R.id.action_food);
+                    case R.id.action_food:
+                        setFrag(0);
+                        break;
+                    case R.id.action_play:
+                        setFrag(1);
+                        break;
+                }
+                return true;
+            }
+        });
+        
+        frag1=new FoodCategory();
+        frag2=new PlayCategory();
+        setFrag(0); // 첫 프래그먼트 화면 지정
+    }
+
+    // 프레그먼트 교체
+    private void setFrag(int n)
+    {
+        System.out.println(n);
+        fm = getSupportFragmentManager();
+        ft= fm.beginTransaction();
+        switch (n)
+        {
+            case 0:
+                ft.replace(R.id.Main_Frame,frag1);
+                ft.commit();
+                break;
+
+            case 1:
+                ft.replace(R.id.Main_Frame,frag2);
+                ft.commit();
+                break;
+        }
+    }
+}
