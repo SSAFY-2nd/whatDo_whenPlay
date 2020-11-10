51d87940e3f8ae0381edf42792cf298efa78bf52
우주호 <einjuho7@ajou.ac.kr>
2020-11-10 13:35:19 +0900

    회원가입 및 로그인 기능 구현

--- Frontend/LoginRegisterExample/app/src/main/java/com/example/loginregisterexample/Frag2.java
+++ Frontend/LoginRegisterExample/app/src/main/java/com/example/loginregisterexample/Frag2.java
@@ -17,8 +17,16 @@ public class Frag2 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        view = inflater.inflate(R.layout.playwithme_likely,container,false);
+        view = inflater.inflate(R.layout.playwithme,container,false);
 
         return view;
     }
+    // ID 입력하여 존재하는 Email인지 확인.
+
+    // 역은 강남역으로 FIX
+
+    
+
+
+
 }
