a2dcfc7c6c98529dd5dd2cf2994d1362013ffa73
임건호 <ghlim001@naver.com>
2020-11-11 01:35:54 +0000

    Update Frontend/LoginRegisterExample/app/src/main/java/com/example/loginregisterexample/Frag2.java

--- Frontend/LoginRegisterExample/app/src/main/java/com/example/loginregisterexample/Frag2.java
+++ Frontend/LoginRegisterExample/app/src/main/java/com/example/loginregisterexample/Frag2.java
@@ -17,7 +17,7 @@ public class Frag2 extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState)
     {
-        view = inflater.inflate(R.layout.detail,container,false);
+        view = inflater.inflate(R.layout.playwithme_likely,container,false);
 
         return view;
     }
