dfd00f5f5692162fe167fd6f248cb43d4c16e6d2
우주호 <einjuho7@ajou.ac.kr>
2020-11-11 10:20:01 +0900

    ADD : playwithme package

--- Frontend/LoginRegisterExample/app/src/main/java/com/example/loginregisterexample/MainActivity.java
+++ Frontend/LoginRegisterExample/app/src/main/java/com/example/loginregisterexample/MainActivity.java
@@ -14,6 +14,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import com.example.loginregisterexample.Playwithme.Frag2;
 import com.example.loginregisterexample.Register.RegisterActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
